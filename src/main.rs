c38dc365bd6d609b7a349305f7155aceb36c4709
faithcredit <honest.dedicate@gmail.com>
2023-07-15 11:49:49 +0900

    one client echo server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
+    let (reader, mut writer) = socket.split();
+
+    let mut reader = BufReader::new(reader);
+
+    let mut line = String::new();
+
+    loop {
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
